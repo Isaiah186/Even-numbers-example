2ba4b8ef82d2e55a82c5618697e44b1bc80d207b
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-09-24 17:53:31 -0500

    Add files via upload

---
+++ Even number example.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+list2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+
+print(list2)
+
+
+# In[7]:
+
+
+for x in list2:
+    if x%2==0:
+        print(x)
+
+
+# In[8]:
+
+
+for y in list2:
+    if y%3==0:
+        print(y)
+
+
+# In[10]:
+
+
+for y in list2:
+    if y%5==0:
+        print(y)
+        
+
+
+# In[ ]:
+
+
+
+
